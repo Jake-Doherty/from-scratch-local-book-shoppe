ad36d46499d104328bbdb7e820c8a856197ac9ce
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-11-09 18:21:50 -0800

    add test for /books route

---
+++ __tests__/books.test.js
@@ -0,0 +1,24 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('/books should return a list of all books and the authors', async () => {
+    const resp = await request(app).get('/books');
+    expect(resp.status).toBe(200);
+    expect(resp.body.length).toBe(8);
+    expect(resp.body[0]).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      released: expect.any(Number),
+      authors: expect.any(Array),
+    });
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
