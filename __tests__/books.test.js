1b32bfc5d2cd8cdc7def3ff38adee87d0408db87
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-11-09 19:05:33 -0800

    add test for book/:id route

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -7,6 +7,16 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  it('books/:id should return a book detail by id', async () => {
+    const resp = await request(app).get('/books/1');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      released: expect.any(Number),
+      authors: expect.any(Array),
+    });
+  });
   it('/books should return a list of all books and the authors', async () => {
     const resp = await request(app).get('/books');
     expect(resp.status).toBe(200);
